ca98fdf037d97845f4d0a0df088c8d42eefa996a
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-08-02 18:23:01 +0600

    init app, устоновил express ,caesar-salad

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const Vigenere = require('caesar-salad').Vigenere;
+const port = 2000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!!!\n');
+});
+
+app.get('/:text', (req, res) => {
+    const MainText = req.params.text;
+    res.send(MainText);
+});
+
+app.get(`/encode/:text`, (req, res) => {
+    const text1 = req.params.text;
+    const encodeText = Vigenere.Cipher('password').crypt(text1);
+    res.send(encodeText);
+});
+
+app.get(`/decode/:text`, (req, res) => {
+    const text2 = req.params.text;
+    const decodeText = Vigenere.Decipher('password').crypt(text2);
+    res.send(decodeText);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at  http://localhost:${port}`);
+});
+
